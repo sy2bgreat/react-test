6e144b73f96ca692709865ec7efcdda2e8f5cd9b
soyoonlee <soyoonlee@omniverse.kr>
2020-11-26 16:56:08 +0900

    스택플로우 힘을 빌어 고쳐보았는데 타입스크립트 훈련이 많이 필요할거 같습니다..

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { setupMaster } from 'cluster';
 import React, { useState } from 'react'
-
+import './App.css'
 
 
 function App() {
   const [show,funcShow] =useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Hello World</h1>
-      </header>
-      <button onClick={()=>funcShow(!show)}>Functional Comp</button>
+      <button onClick={()=>funcShow(!show)}>See</button>
       {show ? <FuncComp></FuncComp> : null}
       <UrName></UrName>
       </div>
@@ -19,41 +15,39 @@ function App() {
 
 
 const  FuncComp = () => {
+  const [date, setDate] = useState((new Date()));
 
-const [date, setDate] = useState((new Date()));
-
- return(
-<div className="container">
+  return(
+  <div className="container">
   <h2>Functional Style Component</h2>
-<p>Date : {date.toString()} </p>
-<button value="new date" onClick={ () => { 
+  <p>Date : {date.toString()} </p>
+  <button value='new Date' onClick={ () => { 
   setDate(new Date())}}>Date Reload</button>
-</div>    
+  </div>    
   );
-}
+};
 
 const UrName = () => {
+
   const [inputValue, setValue] = useState("");
   const [name,setName] =useState("");
 
-  setUser = (e) => {
-    setValue(e.target.value)
-  }
-
-  upDateName = (e) => {
-    e.preventDefault();
+  const setUser = (Event: React.ChangeEvent<HTMLInputElement>):void => {
+    setValue(Event.target.value);
+    };
+  const updateName = (Event:React.MouseEvent) => {
+    Event.preventDefault();
     setName(inputValue);
     setValue("");
-  }
-
-return(
-  <form>
-    <p>Your Name ; {name}</p>
+    };
+  return(
+    <form>
+    <p>Your Name : {name}</p>
     <input type="text" value={inputValue} onChange={setUser}></input>
-    <button onClick={upDateName}>Save</button>
-  </form>
+    <button onClick={updateName}>Save</button>
+    </form>
 
-  )
+  );
 
 };
 
