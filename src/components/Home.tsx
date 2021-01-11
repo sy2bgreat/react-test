ade2a6d9154b6c80ed85873963b986136a205aa2
soyoonlee <soyoonlee@omniverse.kr>
2021-01-11 17:14:46 +0900

    first commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from 'react';
+import { cart, cartState, addFood } from '../store/atom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import apple from '@assets/icons/apple.svg';
+import carrot from '@assets/icons/carrot.svg';
+import hotdog from '@assets/icons/hotdog.svg';
+import iceCream from '@assets/icons/icecream.svg';
+import pepperHot from '@assets/icons/pepper-hot.svg';
+import pizzaSlice from '@assets/icons/pizza-slice.svg';
+
+const ids = [
+  { name: 'Apple', src: apple },
+  { name: 'Carrot', src: carrot },
+  { name: 'Hotdog', src: hotdog },
+  { name: 'Ice cream', src: iceCream },
+  { name: 'Pepper', src: pepperHot },
+  { name: 'Pizze Slice!', src: pizzaSlice },
+];
 
 const Home = () => {
+  const { totalQty } = useRecoilValue(cartState);
+  const setAddFood = useSetRecoilState(addFood);
   return (
     <section className="home-container">
-      <h1>This is HoME</h1>
+      <div className="home-card_container">
+        {ids.map((list, index) => (
+          <div className="home-card" key={index}>
+            <img src={list.src} className="home-card_img"></img>
+            <h3>{list.name}</h3>
+            <button onClick={() => setAddFood(list)}>add</button>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/store/atom.tsx
@@ -0,0 +1,23 @@
+import { atom, selector } from 'recoil';
+
+export const cart = atom({
+  key: 'cart',
+  default: [],
+});
+
+export const cartState = selector({
+  key: 'cartState',
+  get: ({ get }) => {
+    const totalQty = get(cart).reduce((a, b) => a + b, 0);
+    return {
+      totalQty,
+    };
+  },
+});
+
+export const addFood = selector({
+  key: 'addFood',
+  set: ({ set, get }, newFood) => {
+    set(cart, [...get(cart), newFood]);
+  },
+});
